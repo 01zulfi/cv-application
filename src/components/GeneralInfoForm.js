be9d180e6a347509e5330152d95fc93cbe65e2c6
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-11-19 13:29:22 +0500

    Add inputHandler

--- src/components/GeneralInfoForm.js
+++ src/components/GeneralInfoForm.js
@@ -2,9 +2,35 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class GeneralInfoForm extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.generalInfoData = {
+      firstName: "", lastName: "", email: "", phone: "",
+    };
+
+    this.inputHandler = this.inputHandler.bind(this);
+  }
+
+  inputHandler(event) {
     const { dataHandler } = this.props;
 
+    if (event.target.id === "first-name-input") {
+      this.generalInfoData.firstName = event.target.value;
+    }
+    if (event.target.id === "last-name-input") {
+      this.generalInfoData.lastName = event.target.value;
+    }
+    if (event.target.id === "email-input") {
+      this.generalInfoData.email = event.target.value;
+    }
+    if (event.target.id === "phone-number-input") {
+      this.generalInfoData.phone = event.target.value;
+    }
+    dataHandler("general", this.generalInfoData);
+  }
+
+  render() {
     return (
       <div>
         <h2>
@@ -13,19 +39,19 @@ class GeneralInfoForm extends Component {
         <form>
           <label htmlFor="first-name-input">
             First Name:
-            <input type="text" id="first-name-input" onChange={dataHandler} />
+            <input type="text" id="first-name-input" onChange={this.inputHandler} />
           </label>
           <label htmlFor="last-name-input">
             Last Name:
-            <input type="text" id="last-name-input" onChange={dataHandler} />
+            <input type="text" id="last-name-input" onChange={this.inputHandler} />
           </label>
           <label htmlFor="email-input">
             Email:
-            <input type="email" id="email-input" onChange={dataHandler} />
+            <input type="email" id="email-input" onChange={this.inputHandler} />
           </label>
           <label htmlFor="phone-number-input">
             Phone Number:
-            <input type="text" id="phone-number-input" onChange={dataHandler} />
+            <input type="text" id="phone-number-input" onChange={this.inputHandler} />
           </label>
         </form>
       </div>
