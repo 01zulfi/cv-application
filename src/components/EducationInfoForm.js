4f1769e381b83e2a981beabe54e11bb6ced67029
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-11-24 15:15:10 +0500

    Refactor EducationInfoForm to use functional components

--- src/components/EducationInfoForm.js
+++ src/components/EducationInfoForm.js
@@ -1,47 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "../styles/EducationInfoForm.css";
 
 const uniqueId = () => Math.floor(Math.random() * Date.now());
 
-class EducationInfoForm extends Component {
-  constructor(props) {
-    super(props);
+const EducationInfoForm = function ({ dataHandler }) {
+  const [data, setData] = useState([]);
 
-    this.state = {
-      data: [],
-    };
-
-    this.removeForm = this.removeForm.bind(this);
-    this.incrementQuantity = this.incrementQuantity.bind(this);
-    this.inputHandler = this.inputHandler.bind(this);
-    this.isPresentHandler = this.isPresentHandler.bind(this);
-  }
-
-  removeForm(event) {
-    const { dataHandler } = this.props;
+  const removeForm = (event) => {
     const id = event.target.parentNode.getAttribute("data-id");
-    const { data } = this.state;
     const newData = data.filter((el) => el.id !== Number(id));
 
     dataHandler("education", newData);
-    this.setState({
-      data: newData,
-    });
-  }
+    setData(newData);
+  };
 
-  incrementQuantity() {
-    const { data } = this.state;
+  const incrementQuantity = () => {
+    setData(data.concat({ id: uniqueId() }));
+  };
 
-    this.setState({
-      data: data.concat({ id: uniqueId() }),
-    });
-  }
-
-  inputHandler(event) {
-    const { dataHandler } = this.props;
+  const inputHandler = (event) => {
     const id = Number(event.target.closest("form").getAttribute("data-id"));
-    const { data } = this.state;
     let newData = [];
 
     if (event.target.id === "education-qualification") {
@@ -82,20 +61,16 @@ class EducationInfoForm extends Component {
     }
 
     dataHandler("education", newData);
-    this.setState({
-      data: newData,
-    });
-  }
+    setData(newData);
+  };
 
-  isPresentHandler(event) {
+  const isPresentHandler = (event) => {
     if (event.target.id !== "is-present") return;
 
     const { checked } = event.target;
     const form = event.target.closest("form");
     const id = Number(form.getAttribute("data-id"));
-    const { data } = this.state;
     let newData = [];
-    const { dataHandler } = this.props;
     const endDateInput = form.querySelector("#education-end-date");
 
     if (!checked) {
@@ -110,7 +85,6 @@ class EducationInfoForm extends Component {
       });
     } else {
       endDateInput.setAttribute("disabled", "true");
-
       newData = data.map((el) => {
         const newObj = { ...el };
         if (newObj.id === id) {
@@ -121,41 +95,35 @@ class EducationInfoForm extends Component {
     }
 
     dataHandler("education", newData);
-    this.setState({
-      data: newData,
-    });
-  }
+    setData(newData);
+  };
 
-  render() {
-    const { data } = this.state;
-
-    const form = (id) => (
-      <form data-id={id}>
-        Qualification:
-        <input type="text" id="education-qualification" onChange={this.inputHandler} />
-        Institute:
-        <input type="text" id="education-institute" onChange={this.inputHandler} />
-        From:
-        <input type="date" id="education-start-date" onChange={this.inputHandler} />
-        To:
-        <input type="date" id="education-end-date" onChange={this.inputHandler} />
-        <div className="is-present-checkbox">
-          Present
-          <input type="checkbox" id="is-present" onChange={this.isPresentHandler} />
-        </div>
-        <button type="button" onClick={this.removeForm}>Delete</button>
-      </form>
-    );
-
-    return (
-      <div className="education-info-form-div">
-        <h2>Education</h2>
-        <button type="button" onClick={this.incrementQuantity}>Add Education</button>
-        {data.map((el) => <div key={el.id}>{form(el.id)}</div>)}
+  const form = (id) => (
+    <form data-id={id}>
+      Qualification:
+      <input type="text" id="education-qualification" onChange={inputHandler} />
+      Institute:
+      <input type="text" id="education-institute" onChange={inputHandler} />
+      From:
+      <input type="date" id="education-start-date" onChange={inputHandler} />
+      To:
+      <input type="date" id="education-end-date" onChange={inputHandler} />
+      <div className="is-present-checkbox">
+        Present
+        <input type="checkbox" id="is-present" onChange={isPresentHandler} />
       </div>
-    );
-  }
-}
+      <button type="button" onClick={removeForm}>Delete</button>
+    </form>
+  );
+
+  return (
+    <div className="education-info-form-div">
+      <h2>Education</h2>
+      <button type="button" onClick={incrementQuantity}>Add Education</button>
+      {data.map((el) => <div key={el.id}>{form(el.id)}</div>)}
+    </div>
+  );
+};
 
 EducationInfoForm.propTypes = {
   dataHandler: PropTypes.func,
